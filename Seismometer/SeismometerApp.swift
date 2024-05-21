6ad9d87a16ef4f80d1bf87d7b632a4f39f7e5fbb
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-21 12:01:21 -0300

    Initial Commit

---
+++ Seismometer/SeismometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeismometerApp.swift
+//  Seismometer
+//
+//  Created by Christian Manzaraz on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SeismometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
